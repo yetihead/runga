06c675fd49bde9e6843eb0b6f573a917a261a44a
Sergei Khlupin <yetibrain@yandex.ru>
2021-04-18 20:29:46 +0300

    add jest to project

---
+++ src/base-canvas/__tests__/extendable-html-canvas-element.spec.ts
@@ -0,0 +1,43 @@
+import { ExtendableHTMLCanvasElement } from '../extendable-html-canvas-element';
+
+describe('ExtendableHTMLCanvasElement', () => {
+  it('should create instance using `document.createElement method`', () => {
+    const spy = jest
+      .spyOn(global.document, 'createElement')
+      .mockReturnValue({} as HTMLCanvasElement);
+
+    new ExtendableHTMLCanvasElement();
+
+    expect(spy).toBeCalledWith('CANVAS');
+
+    spy.mockClear();
+  });
+
+  it('should be the same object which was returned from `createElement`', () => {
+    const theNewCanvas = {};
+
+    const spy = jest
+      .spyOn(global.document, 'createElement')
+      .mockReturnValue(theNewCanvas as HTMLCanvasElement);
+
+    const instance = new ExtendableHTMLCanvasElement();
+
+    expect(instance).toBe(theNewCanvas);
+
+    spy.mockClear();
+  });
+
+  it('should has right proto', () => {
+    const spy = jest
+      .spyOn(global.document, 'createElement')
+      .mockReturnValue({} as HTMLCanvasElement);
+
+    const instance = new ExtendableHTMLCanvasElement();
+
+    expect(
+      ExtendableHTMLCanvasElement.prototype === Object.getPrototypeOf(instance)
+    ).toBeTruthy();
+
+    spy.mockClear();
+  });
+});
